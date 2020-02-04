e279a940c9796864417ba230253ad9696fb9a15f
Bjorn Lu <bjornlu.dev@gmail.com>
2020-02-04 22:08:05 +0800

    Setup knex migrations

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+require('ts-node/register')
+
+module.exports = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    tableName: 'knex_migrations',
+    extension: 'ts',
+    stub: 'migrations/stub.ts'
+  }
+}
