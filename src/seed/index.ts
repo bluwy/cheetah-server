da463c0d3047a00024844cb99c6b5b15d2adc4aa
bluwy <bjornlu.dev@gmail.com>
2020-08-08 19:55:26 +0800

    Seed job too

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,11 +1,19 @@
 import { seedAdmin } from './admin'
-import { seedStaff } from './staff'
 import { seedCompany } from './company'
 import { seedCustomer } from './customer'
+import { seedJob } from './job'
+import { seedStaff } from './staff'
 
+/**
+ * Seeds the database. Make sure Objection's knex is initialized and the
+ * database is migrated before seeding.
+ */
 export async function seedDatabase() {
-  await seedCompany()
+  // NOTE: Call order matters due to relations. Luckily there's no circular
+  // references so no weird workarounds happening.
   await seedAdmin()
   await seedStaff()
+  await seedCompany()
   await seedCustomer()
+  await seedJob()
 }
