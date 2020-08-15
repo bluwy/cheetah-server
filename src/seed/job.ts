b6930fa51bb5d252c4a7c461cad3d8fb554d7570
bluwy <bjornlu.dev@gmail.com>
2020-08-15 21:37:50 +0800

    Add more job seed

--- src/seed/job.ts
+++ src/seed/job.ts
@@ -1,4 +1,4 @@
-import { Job } from '../models/Job'
+import { Job, JobState } from '../models/Job'
 import { Customer } from '../models/Customer'
 import { Staff } from '../models/Staff'
 import { TaskType } from '../models/Task'
@@ -23,7 +23,7 @@ const jobs = [
   },
   // Current WIP job
   {
-    address: 'ahhhh',
+    address: 'world',
     code: 'K2',
     startDate: new Date(),
     checkIn: new Date(),
@@ -32,8 +32,54 @@ const jobs = [
     tasks: [
       {
         type: TaskType.Service,
-        remarks: 'fix this',
+        remarks: 'fix that',
         done: false
+      }
+    ],
+    actions: []
+  },
+  // Done
+  {
+    address: 'earth',
+    code: 'K3',
+    startDate: new Date(),
+    checkIn: new Date(),
+    // 1 hour
+    checkOut: new Date(Date.now() + 1 * 60 * 60 * 1000),
+    customerIndex: 1,
+    staffPrimaryIndex: 0,
+    staffSecondaryIndex: 1,
+    state: JobState.Done,
+    tasks: [
+      {
+        type: TaskType.Delivery,
+        remarks: 'gift',
+        done: true
+      }
+    ],
+    actions: [
+      {
+        remarks: 'got present back wow'
+      }
+    ]
+  },
+  // Reviewed
+  {
+    address: 'moon',
+    code: 'K4',
+    startDate: new Date(),
+    checkIn: new Date(),
+    // 2 hours
+    checkOut: new Date(Date.now() + 2 * 60 * 60 * 1000),
+    customerIndex: 2,
+    staffPrimaryIndex: 1,
+    staffSecondaryIndex: 0,
+    state: JobState.Reviewed,
+    tasks: [
+      {
+        type: TaskType.Others,
+        remarks: 'fart',
+        done: true
       }
     ],
     actions: []
